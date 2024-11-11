962dae7d8120ba6f1925b8b85abe25ff5ec19a8e
repo <khvits21@gmail.com>
2024-11-11 14:07:33 +0200

    feat: added keyboard with basic functions

--- weather-bot.js
+++ weather-bot.js
@@ -1,10 +1,15 @@
 const { Telegraf } = require('telegraf');
+const { session, Markup } = require('telegraf');
 const axios = require('axios');
 require('dotenv').config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
 
 const bot = new Telegraf(BOT_TOKEN);
+
+const users = {};
+bot.use(session());
+
 
 async function getWeather(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`;
@@ -15,6 +20,17 @@ async function getWeather(city) {
         throw new Error(`Не вдалося отримати погоду для міста "${city}".`)
     }
 }
+
+// Основні опції клавіатури
+const KeyboardOptions = {
+    WEATHER: 'Дізнатись погоду',
+    ADD_CITY: 'Додати місто',
+};
+
+function getMainKeyboard() {
+    return Markup.keyboard([KeyboardOptions.WEATHER, KeyboardOptions.ADD_CITY]).resize();
+}
+
 bot.command('start', (ctx) => {
     ctx.reply('Привіт! Я погодний бот.');
 });
