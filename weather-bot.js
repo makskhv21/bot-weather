0e4a3d4aadec4c95b31f6dfa052dd758e59d4c66
repo <khvits21@gmail.com>
2024-11-11 14:06:31 +0200

    feat: added function to get weather via OpenWeatherMap API

--- weather-bot.js
+++ weather-bot.js
@@ -1,10 +1,20 @@
 const { Telegraf } = require('telegraf');
+const axios = require('axios');
 require('dotenv').config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
+
 const bot = new Telegraf(BOT_TOKEN);
+
+async function getWeather(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`;
+    try {
+        const res = await axios.get(apiUrl);
+        return res.data;
+    } catch (err) {
+        throw new Error(`Не вдалося отримати погоду для міста "${city}".`)
+    }
+}
 bot.command('start', (ctx) => {
     ctx.reply('Привіт! Я погодний бот.');
 });
-bot.launch()
-  .then(() => console.log('Бот запущений'))
-  .catch((err) => console.error('Помилка запуску:', err));
